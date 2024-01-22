3e22d66bdecc872860f745d8c5a8a2ea6646a8a7
Gabriel Al-Rifai <Gabrielalrifai@gmail.com>
2024-01-22 10:49:35 -0600

    Add part 4

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 //Part 1: Refactoring Old Code
 
+const csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+const rows = csvData.split('\n');
+
+for (const row of rows) {
+    const cells = row.split(',');
+
+    console.log(cells[0], cells[1], cells[2], cells[3]);
+}
+
 // Part 2: Expanding Functionality
 
 // Part 3: Transforming Data
